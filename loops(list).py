77188486f27cd4f02f6396c859d4ef7cc7d2e84c
SURAM BABU REDDY <babusuram64@gmail.com>
2026-04-21 08:27:19 +0530

    list(programs)

---
+++ loops(list).py
@@ -0,0 +1,10 @@
+#using for loop to access elements in a list
+a=list(range(10))
+for i in a:
+    print(i)
+#using while loop to access elements in list
+a=list(range(10))
+i=0
+while i<len(a):
+    print(a[i])
+    i+=1
---
+++ practice_programs(list).py
@@ -0,0 +1,11 @@
+a=list(range(1,11,1))
+b=list(range(0,11,2))
+print(a)
+print(b)
+#Accesing the elements by using INDEX
+print(a[0])
+print(b[4])
+#Accesing the elements by Using SLICING
+print(a[0:6:1])
+print(b[::-1])
+print(a[::-1])
